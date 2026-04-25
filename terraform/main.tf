5d9f34f9eb1a1507a814904ba840300259d6c629
alaa kaid ahmed <kaidahmedalaa@gmail.com>
2026-04-25 04:23:10 +0200

    Update EKS node group capacity to 4 nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -254,9 +254,9 @@ resource "aws_eks_node_group" "infoline_nodes" {
   ]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 1
+    desired_size = 4
+    max_size     = 4
+    min_size     = 3
   }
 
 
